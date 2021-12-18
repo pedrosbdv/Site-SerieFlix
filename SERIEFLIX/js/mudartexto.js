2eb6c1cf863e72c751162bb718b3b303174a72f6
Pedro Santos Basilio <96310056+pedrosbdv@users.noreply.github.com>
2021-12-17 22:34:07 -0300

    Add files via upload

---
+++ SERIEFLIX/js/mudartexto.js
@@ -0,0 +1,70 @@
+var titulofilm = document.getElementById("titulofilm");
+var descricaofilm = document.getElementById("descricaofilm");
+var principalfilm = document.getElementById("principalfilm");
+
+function ossimpsons()
+{
+    titulofilm.innerHTML =  "Os Simpsons (1989)";
+    descricaofilm.innerHTML = "A família Simpsons mora na cidade de Springfield, uma cidade americana como qualquer outra. Homer trabalha como inspetor de segurança numa usina de energia nuclear. Marge tenta manter a paz em sua família. Bart é um travesso garoto de 10 anos. Lisa, 8, é a inteligente da família, que adora tocar saxofone e é vegetariana. E a pequena Maggie conquista todos enquanto não larga sua chupeta. E a série tem um rico, e muitas vezes estranho, universo de personagens que também habitam Springfield. Com roteiros inteligentes, humor subversivo e divertidamente genial, a série faz piadas de si mesma e de todo mundo que aparecer em seu caminho.";
+    principalfilm.style.backgroundImage = "linear-gradient(rgba(0,0,0,.50), rgba(0,0,0,.50)100%), url('img/principalfilm/ossimpsons.jpg')";
+}
+
+function odeiaocris ()
+{
+    titulofilm.innerHTML =  "Todo Mundo Odeia o Chris (2005)"
+    descricaofilm.innerHTML = "Um adolescente negro do Brooklyn durante a década de 80 que frequenta uma escola onde a maioria dos alunos são brancos."
+    principalfilm.style.backgroundImage = "linear-gradient(rgba(0,0,0,.50), rgba(0,0,0,.50)100%), url('img/principalfilm/odeiaocris.jpg')";
+}
+
+function malhacao ()
+{
+    titulofilm.innerHTML =  "Malhação (1995)"
+    descricaofilm.innerHTML = "Série de televisão brasileira produzida e exibida pela TV Globo desde 24 de abril de 1995. É baseada nos moldes estadunidense de soap opera – um formato similar às telenovelas, porém com temporadas que se estendem por anos – sendo voltada para o público adolescente."
+    principalfilm.style.backgroundImage = "linear-gradient(rgba(0,0,0,.50), rgba(0,0,0,.50)100%), url('img/principalfilm/malhacao.jpg')";
+}
+
+function chaves ()
+{
+    titulofilm.innerHTML =  "Chaves (1973)"
+    descricaofilm.innerHTML = "Criado por Roberto Gómes Bolaños, Chaves fez sucesso no Brasil como uma comédia leve e divertida sobre moradores de um vila cheia de confusões. Chaves, Chiquinha, Quico, Seu Madruga, Dona Florina, Bruxa do 71 e muitos outros fazem parte desse seriado divertido"
+    principalfilm.style.backgroundImage = "linear-gradient(rgba(0,0,0,.50), rgba(0,0,0,.50)100%), url('img/principalfilm/chaves.jpg')";
+}
+function chapolincolorado ()
+{
+    titulofilm.innerHTML =  "Chapolin Colorado (1972)"
+    descricaofilm.innerHTML = "O Chapolin Colorado é um herói atrapalhado, medroso e desastrado, que sempre aparece quando alguém precisa. Com sua marreta biônica e a pílula encolhedora, ele, muitas vezes, provoca mais confusão do que necessariamente ajuda."
+    principalfilm.style.backgroundImage = "linear-gradient(rgba(0,0,0,.50), rgba(0,0,0,.50)100%), url('img/principalfilm/chapolincolorado.jpg')";
+}
+function picapau ()
+{
+    titulofilm.innerHTML =  "Pica-Pau (1999)"
+    descricaofilm.innerHTML = "Acompanhe as aventuras de um pássaro muito engraçado que sempre se envolve em situações divertidas."
+    principalfilm.style.backgroundImage = "linear-gradient(rgba(0,0,0,.50), rgba(0,0,0,.50)100%), url('img/principalfilm/picapau.jpg')";
+}
+function otemponaopara ()
+{
+    titulofilm.innerHTML =  "O Tempo Não Para (2018)"
+    descricaofilm.innerHTML = "A família Sabino Machado é proprietária de diversas terras para exploração de ouro e minérios e investimentos em telefonia, uma das mais influentes da São Paulo de 1886. Dom Sabino tem um plano de conhecer o novo estaleiro comprado na Inglaterra, ao mesmo tempo que mantem a filha Marocas longe da cidade, depois que ela recusou um casamento no altar. Para concretizar o propósito, toda a família viaja a bordo do Albatroz, um dos mais seguros navios da época, rumo à Europa. Após bater em um iceberg, a embarcação naufraga e muitos passageiros acabam congelando por causa da baixa temperatura da água. 132 anos depois, o empresário Samuca observa um enorme bloco de gelo se aproximando da praia do Guarujá, em São Paulo, enquanto surfa. São os Sabino Machado, que depois de descongelados, terão de enfrentar a realidade contemporânea completamente nova para eles."
+    principalfilm.style.backgroundImage = "linear-gradient(rgba(0,0,0,.50), rgba(0,0,0,.50)100%), url('img/principalfilm/otemponaopara.jpg')";
+}
+function policia ()
+{
+    titulofilm.innerHTML =  "P.O.L.I.C.I.A (2017)"
+    descricaofilm.innerHTML = "Veja o trabalho de patrulhamento, batidas e confrontos que podem custar a vida de policiais na sua rotina diária contra o crime."
+    principalfilm.style.backgroundImage = "linear-gradient(rgba(0,0,0,.50), rgba(0,0,0,.50)100%), url('img/principalfilm/policia.jpg')";
+}
+function masterchef ()
+{
+    titulofilm.innerHTML =  "Masterchef Brasil (2014)"
+    descricaofilm.innerHTML = "O maior reality de culinária do mundo revela os novos talentos da cozinha brasileira."
+    principalfilm.style.backgroundImage = "linear-gradient(rgba(0,0,0,.50), rgba(0,0,0,.50)100%), url('img/principalfilm/masterchef.jpg')";
+}
+function apracaenossa ()
+{
+    titulofilm.innerHTML =  "A Praça é Nossa"
+    descricaofilm.innerHTML = "A Praça é Nossa é uma comédia brasileira transmitida pelo SBT. Foi criada em 1956 por Manuel de Nóbrega, com o nome de A Praça da Alegria. Baseia-se em um cenário simples e prático: um banco em um jardim quadrangular, por onde passam vários personagens."
+    principalfilm.style.backgroundImage = "linear-gradient(rgba(0,0,0,.50), rgba(0,0,0,.50)100%), url('img/principalfilm/apracaenossa.jpg')";
+}
+
+
+
